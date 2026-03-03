ce0f7b0f48042ac4f0635064a84c282daa1495e6
Isaias Solano <114197516+IsaiasCoder@users.noreply.github.com>
2026-03-02 23:04:27 -0600

    feat: add conditional form

--- cost-per-time-angular/src/app/features/calculator/form/form.ts
+++ cost-per-time-angular/src/app/features/calculator/form/form.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-form',
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './form.css',
 })
 export class Form {
-
+  activeTab = signal<'existing' | 'custom'>('custom');
 }
--- cost-per-time-angular/src/app/features/home/welcome/welcome.ts
+++ cost-per-time-angular/src/app/features/home/welcome/welcome.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-
+import { Form } from '../../calculator/form/form';
 @Component({
   selector: 'app-welcome',
-  imports: [],
+  imports: [Form],
   templateUrl: './welcome.html',
   styleUrl: './welcome.css',
 })
-export class Welcome {
-  
-}
+export class Welcome {}
